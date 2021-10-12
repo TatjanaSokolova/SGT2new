023668c390253801f2cb092a89f9912cf5ed28da
TatjanaSokolova <tsvm@inbox.lv>
2021-10-12 17:58:39 +0300

    lesson 5 updated

--- src/lesson4/Values.java
+++ src/lesson4/Values.java
@@ -1,11 +1,12 @@
 package lesson4;
+
 import java.util.TreeSet;
 
 public class Values {
     public static String maxvalues(Integer nums[]) {
         TreeSet<Integer> set = new TreeSet<Integer>();
 
-        for(int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             set.add(nums[i]);
         }
 
@@ -15,22 +16,20 @@ public class Values {
         System.out.println(set);
         if (nums2.length == 0) {
             return "Array is empty";
-        }
-        else if (nums2.length > 2) {
-            return String.format("The third distinct maximum is %s", nums2[nums2.length-3]);
-        }
-        else {
-            return String.format("The first distinct maximum is: %s", nums2[nums2.length-1]);
+        } else if (nums2.length > 2) {
+            return String.format("The third distinct maximum is %s", nums2[nums2.length - 3]);
+        } else {
+            return String.format("The first distinct maximum is: %s", nums2[nums2.length - 1]);
         }
     }
 
     public static void main(String[] args) {
         System.out.println("Test #1");
-        Integer arr[] = {3,5,66,45,23};
+        Integer arr[] = {3, 5, 66, 45, 23};
         System.out.println(maxvalues(arr));
 
         System.out.println("Test #2");
-        Integer arr2[] = {-1, -4, -6, -99};
+        Integer arr2[] = {99, -4, -6, -99};
         System.out.println(maxvalues(arr2));
 
         System.out.println("Test #3");
@@ -38,7 +37,7 @@ public class Values {
         System.out.println(maxvalues(arr3));
 
         System.out.println("Test #4");
-        Integer arr4[] = {1,1, 2, 2, 2, 56, 76};
+        Integer arr4[] = {1, 1, 2, 2, 2, 56, 76};
         System.out.println(maxvalues(arr4));
 
         System.out.println("Test #5");
