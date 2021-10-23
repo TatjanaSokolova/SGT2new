dbdc1d9ec9b08193b54b837194550e2ad136e63f
TatjanaSokolova <tsvm@inbox.lv>
2021-10-23 18:25:40 +0300

    lesson9

---
+++ portfolio/src/main/java/lv/tanja/portfolio/Controllers/FrontPageController.java
@@ -0,0 +1,9 @@
+package lv.tanja.portfolio.Controllers;
+
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.ServletContext;
+
+@Controller
+public class FrontPageController {
+    }
---
+++ portfolio/src/main/resources/static/Scripts.js
@@ -0,0 +1,10 @@
+let blueButton = document.getElementById("blue-button");
+
+
+function colorBlue() {
+    document.body.style.backgroundColor = "blue";
+}
+let pinkButton = document.getElementById("pink-button");
+function colorPink() {
+    document.body.style.backgroundColor = "pink";
+}
